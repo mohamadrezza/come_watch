d700023ba587f2258a1f9c7aea2798672e189a57
unknown <7enson77@gmail.com>
2019-10-11 23:09:55 +0330

    shahrdl

--- crawler/shahrdl.js
+++ crawler/shahrdl.js
@@ -1,63 +1,102 @@
-const cheerio = require('cheerio')
+var cheerio = require('cheerio')
 var helper = require('../helpers/helpers')
 var rp = require('request-promise')
-var fs = require('fs')
-var source = 'http://dl11.difid.ir'
-var results = []
-var errors = []
-rp(source).then(htmlString => {
-    const $ = cheerio.load(htmlString)
-    $('a').each(async (i) => {
-        try {
-            var urls = $('a').eq(i).attr('href');
-            var movieLinks = source + urls
-            if (urls == '/web.config' || urls == '/') {
-                return false;
-            }
-            var link = await rp(movieLinks)
-            var page = cheerio.load(link)
-            page('a').each(async test => {
-                var route = page('a').eq(test).attr('href')
-                var movieLinks = source + route
-                if (route == '/' || route == '/L/Alarm/') {
-                    return false
+
+// another loop ?! kiss my ass ... 
+var sources = [
+    'http://dl11.difid.ir/0-9/',
+    'http://dl11.difid.ir/B/',
+    'http://dl11.difid.ir/C/',
+    'http://dl11.difid.ir/D/',
+    'http://dl11.difid.ir/E/',
+    'http://dl11.difid.ir/F/',
+    'http://dl11.difid.ir/G/',
+    'http://dl11.difid.ir/H/',
+    'http://dl11.difid.ir/I/',
+    'http://dl11.difid.ir/J/',
+    'http://dl11.difid.ir/K/',
+    'http://dl11.difid.ir/L/',
+    'http://dl11.difid.ir/M/',
+    'http://dl11.difid.ir/N/',
+    'http://dl11.difid.ir/O/',
+    'http://dl11.difid.ir/P/',
+    'http://dl11.difid.ir/Q/',
+    'http://dl11.difid.ir/R/',
+    'http://dl11.difid.ir/S/',
+    'http://dl11.difid.ir/T/',
+    'http://dl11.difid.ir/U/',
+    'http://dl11.difid.ir/V/',
+    'http://dl11.difid.ir/W/',
+    'http://dl11.difid.ir/X/',
+    'http://dl11.difid.ir/Y/',
+    'http://dl11.difid.ir/Z/',
+]
+//there is some extra route and web.configs and i don't want loop anymore :D
+async function job() {
+    try {
+        sources.forEach(async source => {
+            var data = await rp(source)
+            var htmlPage = cheerio.load(data)
+            htmlPage('a').each(async i => {
+                try {
+                    var yearLink = htmlPage('a').eq(i).text()
+                    if (yearLink != '[To Parent Directory]') {
+                        var link = await rp(source + yearLink)
+                        var page = cheerio.load(link)
+                        page('a').each(item => {
+                            var movie = page('a').eq(item).text()
+                            let year = helper.getYearFromMovieName(movie);
+                            let parsedName = helper.parseMovieName(movie, year)
+                            var dlLink = source + yearLink + '/' + movie
+                            if (movie != '[To Parent Directory]') {
+                                var result = {
+                                    name: parsedName,
+                                    year: year,
+                                    quality: helper.getQuality(movie),
+                                    release: helper.getRelease(movie),
+                                    dubbed: helper.isDubbed(movie),
+                                    link: dlLink
+                                }
+                            }
+                            // a lot of movies !!
+                            // console.table(result) 
+
+                        })
+                    }
+                } catch (e) {
+                    console.log(e.message)
                 }
-                var dlPage = await rp(movieLinks)
-                var nextPage = cheerio.load(dlPage)
-                nextPage('a').each(async item => {
-                    var name = nextPage('a').eq(item).text()
-                    var link = nextPage('a').eq(item).attr('href')
-                    let year = helper.getYearFromMovieName(name);
-                    let parsedName = helper.parseMovieName(name, year)
-                    var dlLink = source + link
-                    if (link == '/') {
-                        return false
-                    }
-                    var result = {
-                        name: parsedName,
-                        link: dlLink,
-                        year: year,
-                        size: "!!!",
-                        dubbed: helper.isDubbed(name)
-
-                    }
-                    if (!helper.isValidExt(name)) {
-                        errors.push(result)
-                        return false
-                    } else {
-                        results.push(result)
-                      
-                    }
-                })
             })
-    console.log(results)
-            
-        } catch (e) {
-            console.log(e.message)
-        }
+        })
 
-    })
-  
-}).catch(err => {
-    console.log(err.message)
-})
+    } catch (e) {
+        console.log(e.message)
+    }
+}
+job()
+
+
+
+
+
+
+
+
+
+
+//origin feature code
+// function job() {
+//     source.forEach(async item => {
+//         try {
+//             var htmlPage = await rp(item)
+//             console.log(htmlPage)
+//         } catch (e) {
+//             console.log(e.message)
+
+//         }
+
+//     })
+
+
+// }
+// job()
